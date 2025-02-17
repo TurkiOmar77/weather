3a841f0db6160f16992a154562df871eea8cb637
AliBasonbul <alimohmmedbasunbul@gmail.com>
2025-02-17 21:30:04 +0300

    fixed weatherService

--- services/weatherService.ts
+++ services/weatherService.ts
@@ -1,11 +1,11 @@
-import {weatherData} from "../types/weatherType"
+import { weatherData } from "../types/weatherType"
 
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+const BASE_URL = process.env.NEXT_PUBLIC_WEATHER_API_URL;
 
-export const fetchWeather = async (city : string) : Promise<weatherData> =>{
+export const fetchWeather = async (city: string): Promise<weatherData> => {
     const response = await fetch(`${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`)
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error("failed to fetch weather data !!!! test") // يوجد تعديل
     }
     return response.json()
