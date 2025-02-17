33f0a2d815b4c8fe5069ebc08a8990c21c9e44df
TurkiOmar77 <turkiomaralsaar@gmail.com>
2025-02-17 12:46:03 +0300

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -8,17 +8,11 @@ export default function Home() {
   const { weather } = useWeather(city)
   return (
     <div>
-<<<<<<< HEAD
-      <SearchBar onCahnge={setCity} /> 
-=======
       <SearchBar onCahnge={setCity} />
       <h1>{weather?.name}</h1>
       <h1>{weather?.sys.country}</h1>
       <h1>{weather?.main.temp}</h1>
       <h1>{weather?.weather[0].description}</h1>
-
-
->>>>>>> dc6215c4dfb5f0afecc8ce4375935a6915461cb7
       <h1>{city}</h1>
     </div>
   );
